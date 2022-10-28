05198a40e05b54ebf9e98c101b71f2628b4d513b
BossBaby25 <meraj15-4213@diu.edu.bd>
2022-10-28 19:38:53 +0600

    app and authjprovider issue fixed

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthProvider from './Contexts/AuthProvider/AuthProvider';
 import App from './App';
+/* import AuthProvider from './Contexts/AuthProvider/AuthProvider';
+import App from './App'; */
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
