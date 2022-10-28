04e7ae7364ba0b5974651e8a57fc39763fa7a892
BossBaby25 <meraj15-4213@diu.edu.bd>
2022-10-28 19:31:00 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvkEvK6E-Q9_VkvJ-FmdAh6YgwkkQXKVI",
+  authDomain: "binary-brains-bf1e6.firebaseapp.com",
+  projectId: "binary-brains-bf1e6",
+  storageBucket: "binary-brains-bf1e6.appspot.com",
+  messagingSenderId: "622412553816",
+  appId: "1:622412553816:web:40cc60bd0c816cf625342d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
